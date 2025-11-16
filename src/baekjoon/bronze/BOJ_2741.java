ceaa82eed9cfda6ea4fce08dbc008127a7da9910
dohyunk58 <hedge3x@gmail.com>
2025-11-16 23:51:18 +0900

    solved BOJ_2471

---
+++ src/baekjoon/bronze/BOJ_2741.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class BOJ_2741 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
