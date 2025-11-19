1cb632bbf85aa1929dfb5126f4f16e7909757d19
dohyunk58 <hedge3x@gmail.com>
2025-11-19 14:18:50 +0900

    solved BOJ_2018 연속된 자연수의 합 구하기

---
+++ src/baekjoon/silver/BOJ_2018.java
@@ -0,0 +1,35 @@
+package baekjoon.silver;
+
+/*
+1. 1부터 N까지의 배열을 생성하고 배열 시작점에 탐색의 시작과 끝을 가리키는 포인터를 선언한다.
+2. 다음 세 가지 경우로 나누어 포인터를 움직인다.
+    1) 시작~끝까지의 합 < N: 끝 포인터++
+    2) 시작~끝까지의 합 == N: 결과++, 끝 포인터++
+    3) 시작~끝까지의 합 > N: 처음 포인터++
+3. 끝포인터와 시작 포인터가 모두 N에 도달하면 프로그램을 종료한다.
+ */
+
+import java.io.*;
+
+public class BOJ_2018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int front = 1, end = 1;
+        long sum = 1; int count = 0;
+        do {
+            if(sum < n) {
+                end++;
+                sum += end;
+            } else if(sum == n) {
+                count++;
+                end++; sum += end;
+            } else if(sum > n) {
+                sum -= front;
+                front++;
+            }
+        } while(front <= n && end <= n);
+
+        System.out.print(count);
+    }
+}
