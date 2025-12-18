dc821f9cb62474fea03b94ca3768b0ceeacebe3c
dohyunk58 <hedge3x@gmail.com>
2025-12-18 17:46:44 +0900

    solved 2775

---
+++ src/baekjoon/bronze/BOJ_2775.java
@@ -0,0 +1,31 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[15][15];
+        // 0층 초기화
+        for(int j = 1; j < 15; j++) {
+            arr[0][j] = j;
+        }
+
+        for(int i = 1; i < 15; i++) {
+            for(int j = 1; j < 15; j++) {
+                arr[i][j] = arr[i][j-1] + arr[i-1][j];
+            }
+        }
+
+        for(int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            System.out.println(arr[k][n]);
+        }
+    }
+}
