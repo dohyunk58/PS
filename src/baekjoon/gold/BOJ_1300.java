8197ca68f6fed168d71826871dbb85b64f2033ad
dohyunk58 <hedge3x@gmail.com>
2025-12-31 03:35:47 +0900

    solved 1300

---
+++ src/baekjoon/gold/BOJ_1300.java
@@ -0,0 +1,42 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        int start = 1; int end = k; // NxN에서 k번째 수는 k보다 크지는 않음
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            int smallerCnt = findSmallerNumber(n, mid);
+            if (smallerCnt < k) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.print(start);
+    }
+
+    public static int findSmallerNumber (int n, int x) {
+        int smallerCnt = 0;
+        for (int i = 1; i <= n; i++) {
+            int quotient = x / i;
+
+            if (quotient > n) {
+                quotient = n;
+            }
+
+            smallerCnt += quotient;
+        }
+        return smallerCnt;
+    }
+}
