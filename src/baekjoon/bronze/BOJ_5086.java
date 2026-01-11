3b905e1b50a6f83be5c4e2017fe81521174fbb3f
dohyunk58 <hedge3x@gmail.com>
2026-01-12 05:52:55 +0900

    solved 5086

---
+++ src/baekjoon/bronze/BOJ_5086.java
@@ -0,0 +1,24 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_5086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) break;
+
+            if (a % b == 0) System.out.println("multiple");
+            else if (b % a == 0) System.out.println("factor");
+            else System.out.println("neither");
+        }
+    }
+}
