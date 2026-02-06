8655e92a2680d2d9a58cafa5b3aa9ae69f66b256
dohyunk58 <hedge3x@gmail.com>
2026-02-07 05:36:56 +0900

    solved 7568

---
+++ src/baekjoon/silver/BOJ_7568.java
@@ -0,0 +1,47 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] people = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            people[i][0] = w;
+            people[i][1] = h;
+        }
+
+        int[] count = new int[n];
+
+        // 비교
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue; // 자신 제외
+
+                if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+                    count[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append(count[i]+1).append(" ");
+        }
+        System.out.print(sb);
+    }
+
+}
