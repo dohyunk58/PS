eb3fe849f021b6401918f54650b2f1354a817599
dohyunk58 <hedge3x@gmail.com>
2026-03-15 01:08:46 +0900

    solved 15894

---
+++ src/baekjoon/bronze/BOJ_15894.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(4 * Long.parseLong(br.readLine()));
+        br.close();
+    }
+}
