152fe77f51c231511e34978d37a6038b79ba72fb
dohyunk58 <hedge3x@gmail.com>
2026-03-20 03:56:25 +0900

    solved 11051

---
+++ src/baekjoon/silver/BOJ_11051.java
@@ -0,0 +1,28 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11051 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] D = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) D[i][j] = 1; // nC0 = nCn = 1
+                else if (j == 1) D[i][j] = i; // nC1 = n
+                else D[i][j] = (D[i - 1][j] + D[i - 1][j - 1]) % 10_007;
+            }
+        }
+
+        System.out.print(D[n][k]);
+    }
+}
